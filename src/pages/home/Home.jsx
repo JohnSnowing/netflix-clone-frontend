3b3eb58bd64d26fa66c610f005eb57ae7bae51a1
Jonmari <jonmari@simplecloudology.com>
2022-05-03 01:02:56 +0800

    call api

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,42 @@
+import { useEffect, useState } from "react";
 import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
+import axios from "axios";
 
 const Home = ({ type }) => {
+    const [lists, setLists] = useState([]);
+    const [genre, setGenre] = useState(null);
+
+    useEffect(() => {
+        const getRandomLists = async () => {
+            try {
+                const res = await axios.get(
+                    `lists${type ? "?type=" + type : ""}${
+                        genre ? "&genre=" + genre : ""
+                    }`,
+                    {
+                        headers: {
+                            token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNmFlMmUwNDdlODAyNmVhYzRlMDE4OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1MTUwNjU2MSwiZXhwIjoxNjUxOTM4NTYxfQ.vA3yyVGNeVNVhvfIj1UoP5Hdcum0QtlAuGH3yha7ab4",
+                        },
+                    },
+                );
+                setLists(res.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getRandomLists();
+    }, [type, genre]);
+
     return (
         <div className="home">
             <Navbar />
             <Featured type={type} />
-            <List />
-            <List />
-            <List />
-            <List />
+            {lists.map((list, index) => (
+                <List key={index} list={list} />
+            ))}
         </div>
     );
 };
--- src/pages/watch/Watch.jsx
+++ src/pages/watch/Watch.jsx
@@ -1,19 +1,23 @@
 import { ArrowBackOutlined } from "@material-ui/icons";
+import { Link, useLocation } from "react-router-dom";
 import "./watch.scss";
 
 const Watch = () => {
+    const location = useLocation();
+
+    const movie = location.state;
+    // to get the value of object in the link
+
     return (
         <div className="watch">
-            <div className="back">
-                <ArrowBackOutlined />
-                Home
-            </div>
-            <video
-                className="video"
-                autoPlay
-                controls
-                src="http://127.0.0.1:8887/Spider-Man%20No%20Way%20Home%20(2021)%20%5B720p%5D%20%5BBluRay%5D%20%5BYTS.MX%5D/Spider-Man.No.Way.Home.2021.720p.BluRay.x264.AAC-%5BYTS.MX%5D.mp4"
-            />
+            <Link to="/">
+                <div className="back">
+                    <ArrowBackOutlined />
+                    Home
+                </div>
+            </Link>
+
+            <video className="video" autoPlay controls src={movie.trailer} />
         </div>
     );
 };
